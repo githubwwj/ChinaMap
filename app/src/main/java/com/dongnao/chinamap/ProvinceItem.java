8a1a66dadc47a638f4c9a2bfc6b284414a8feb83
wwj <wwj_computeryes@163.com>
2020-04-28 19:43:11 +0800

    1 添加阴影先注释

--- app/src/main/java/com/dongnao/chinamap/ProvinceItem.java
+++ app/src/main/java/com/dongnao/chinamap/ProvinceItem.java
@@ -55,6 +55,11 @@ public class ProvinceItem {
             paint.setStrokeWidth(2);
             paint.setColor(Color.MAGENTA);
             paint.setShadowLayer(6, 0, 0, Color.BLUE);
+//            radius:模糊半径，radius越大越模糊，越小越清晰，但是如果radius设置为0，则阴影消失不见
+//            dx:阴影的横向偏移距离，正值向右偏移，负值向左偏移
+//            dy:阴影的纵向偏移距离，正值向下偏移，负值向上偏移
+//            color: 绘制阴影的画笔颜色，即阴影的颜色（对图片阴影无效）
+
             canvas.drawPath(mPath, paint);
 
             //选中时，绘制描边效果
